22c2919a9a5e60e54db9ac1788ef4eed372f7e42
Alejo <lperilla2@gmail.com>
2022-08-09 22:07:22 -0500

    prepare to SB

--- javaSrc/firts/src/main/java/net/coffeproject/App.java
+++ javaSrc/firts/src/main/java/net/coffeproject/App.java
@@ -1,6 +1,8 @@
 package net.coffeproject;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
@@ -8,17 +10,26 @@ import org.json.simple.parser.*;
  
 public class App 
 {
-    public static void main( String[] args ){
+    public static void main( String[] args ) throws FileNotFoundException, IOException, ParseException{
     	
     	JSONObject myDrinks;
-        try {
+       
 			 myDrinks = (JSONObject) new JSONParser().parse(new FileReader("E:\\proyecto CofeWeb\\proyectoWeb\\js\\objetosCafe.json"));
 			 System.out.println(myDrinks);
-		} catch (IOException | ParseException e) {
-			
-			e.printStackTrace();
-		}
+		 
+			 //metodo para remplazar el valor de la bebida
+			 if (myDrinks.containsKey("Cafe")) {
+				 myDrinks.put("Cafe", 555);
+			 } 
+			 
+			 //sobreescribe el documento con los nuevos valores 
+			 FileWriter fileWriter = new FileWriter("E:\\proyecto CofeWeb\\proyectoWeb\\js\\objetosCafe.json");
+			    fileWriter.write(myDrinks.toJSONString());
+			    fileWriter.flush();
+			    fileWriter.close();
         
+        System.out.println(myDrinks);
         
+    
     }
 }
